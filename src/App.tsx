e60b8e1be26ef3e61f2fa24d87dd6d1e6b5c95d2
Jairo Koning <jairo.koning@gmail.com>
2023-09-14 09:03:10 -0300

    chore: config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { ThemeProvider } from "styled-components"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Ignite Timer</h1>
       <GlobalStyle />
     </ThemeProvider>
-    
   )
 }
